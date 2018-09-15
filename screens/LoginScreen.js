4075e5d80324db3e98476143769e548c62f673b6
Waseem Koya <waseemkoya@gmail.com>
2018-09-15 22:08:35 +1200

    now able to log in from firebase and push to home screen. Not using LoginForm.js now since for some reason couldn't push to page from that component

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-import LoginForm from '../components/LoginForm';
+import { View, Button, Alert } from 'react-native';
+import { TitledInput } from '../components/TitledInput';
+import firebase from 'firebase';
 
 class LoginScreen extends Component{
 
+    state = { email: '', password: '' };
     static navigationOptions = {
         header: null
     }
     
+    login = () => {
+        if(this.state.email == '' || this.state.password == ''){
+            alert('Please fill in the fields');
+            return;
+        }
+        firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password)
+        .then(() => {this.props.navigation.navigate('Home')})
+        .catch((error) => {alert(error)})
+        ;
+    }
+
     render() {
         return (
             <View>
-                <LoginForm/>
+                <TitledInput 
+                    label='Email Address'
+                    placeholder='you@domain.com'
+                    value={this.state.email}
+                    onChangeText={email => this.setState({ email })}
+                />    
+                <TitledInput 
+                    label='Password'
+                    autoCorrect={false}
+                    placeholder='*******'
+                    secureTextEntry
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })}
+                    />
+                    <Button onPress={this.login} title="Log in" />
+
 				<Button
                  onPress={() => this.props.navigation.navigate('Register')}
 				 title="Register" 
