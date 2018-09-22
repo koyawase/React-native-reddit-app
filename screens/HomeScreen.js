6748d9c57c3dfff78b2437c5a76d3ffbc720a3a8
Waseem Koya <waseemkoya@gmail.com>
2018-09-22 18:07:37 +1200

    pulling data from reddit api. Now need to display it on page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,10 +3,30 @@ import { View, Text } from 'react-native';
 
 class HomeScreen extends Component{
 
+    state = {category: '', likedPosts: []};
     static navigationOptions = {
         tabBarLabel: 'Home',
     }
     
+    componentDidMount(){
+        console.log('Home page loaded');
+        this.state.category = 'popular';
+        this.getRedditPosts();
+    }
+
+     getRedditPosts(){
+        return fetch('https://www.reddit.com/r/'+this.state.category+'/top.json?limit=50')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson.data.children);
+            this.setState( {likedPosts: responseJson.data.children});
+            console.log(this.state.likedPosts);
+        })
+        .catch((error) => {
+            console.log('error ' + error);
+        });
+     }
+
     render() {
 
         return (
